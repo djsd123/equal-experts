775f0c746620a0a483c6e4fa25441c67f9029de4
Mikael Allison <mikellallison@gmail.com>
2022-06-01 08:02:03 +0100

    Initial Commit
    A small command line utility to list a user's public gists

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # equal-experts
 A small command line utility to list a user's public gists
+
+[node]: https://nodejs.org/en/download/package-manager/
+
+Lists a user's public gists.  On subsequent runs will only list a previously queried user's gists that have been published 
+since the last time that user was queried :confused:
+
+## Requirements
+
+| Name     | Version            |
+|----------|--------------------|
+| [node]   | > = 14.x, < 15.x   |
+
+
+## Setup
+
+Once you've installed [node]. Open a shell/terminal at the root of this project, then:
+* `npm install`
+* `npm run build` **Optional** but recommended
+
+**note**
+
+Once you have run `npm install` there should be a tool added to your path called `ts-node-esm`. Which will allow you to exec the typescript directly by passing the [index.ts](index.ts)
+file directly to `ts-node-esm`.  For example `ts-node-esm index.ts .....`.
+
+Alternatively, you can run `npm run build` which will transpile the typescript to javascript allowing you use the [node] runtime
+executable. i.e. `node index.js ....`
+
+I tried to create a static binary and unfortunately failed :disappointed:. See github [issue](https://github.com/vercel/pkg/issues/1291)
+
+## Usage
+
+__Print Help__
+
+
+`npm run help`
+or
+`node index.js --help`
+
+
+
+__List a user's gists__
+
+
+`node index.js -u <USERNAME>`
+
+__List all gists for a previously queried user__
+
+
+`node index.js -u <USERNAME> -f`
+
+
+## Options
+
+| Flag             | Description                                                         | Required |
+|------------------|---------------------------------------------------------------------|----------|
+| `-u, --user`     | `The GitHub handle of user who's gists you'd like to list`          | `false`  |
+| `-f, --freshrun` | `List all of the user's gists even if listed during a previous run` | `false`  |
+| `--version`      | `Show version number`                                               | `false`  |
+| `--help`         | `Show help`                                                         | `false`  |
---
+++ index.ts
@@ -0,0 +1,124 @@
+import { Octokit } from '@octokit/core';
+import chalk from 'chalk';
+import figlet from 'figlet';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers'
+import fs from 'fs';
+import * as os from 'os';
+
+type run = {
+    UserName: string
+    LastRunTimeStamp: string
+}
+const defaultUser = 'takakabe'
+const timeNow = new Date(Date.now()).toISOString()
+
+const options = yargs(hideBin(process.argv)).usage('Usage: node index.js [-u, --user [username]] [-f, --freshrun]')
+    .options('user', {
+        alias: 'u',
+        describe: 'The GitHub handle of user who\'s gists you\'d like to list',
+        type: 'string',
+        default: defaultUser,
+        demandOption: false,
+    })
+    .option('freshrun', {
+        alias: 'f',
+        describe: 'List all of the user\'s gists even if listed during a previous run',
+        type: 'boolean',
+        default: false,
+        demandOption: false,
+    }).help().parseSync()
+
+let filePath: fs.PathLike
+
+switch (os.platform()) {
+    case 'win32':
+        filePath = `${process.env.APPDATA}\\lastRUN`
+        break;
+    default:
+        filePath = `${process.env.HOME}/lastRUN`
+}
+
+const client = new Octokit({})
+
+const getGists = async (user: string, time?: string) => {
+    const results = await client.request('GET /users/{username}/gists', {
+        username: user,
+        since: time,
+    }).then(result => {
+        console.log(chalk.cyan(figlet.textSync(user, { horizontalLayout: 'default', font: 'Colossal' })))
+        result.data.forEach(gist => console.log(
+            chalk.magentaBright('Created: ') + new Date(gist.created_at).toDateString() +
+            chalk.cyan(','), chalk.green('URL: ') + gist.html_url +
+            chalk.cyan(','), chalk.yellow('Description: ') + gist.description
+            )
+        )
+        return
+    }).catch(error => console.log(error))
+}
+
+const runFileExists = (file: fs.PathLike) => {
+    return fs.existsSync(file)
+}
+
+const loadRunFile = (file: fs.PathLike): run[] => {
+    return JSON.parse(fs.readFileSync(file, 'utf-8'))
+}
+
+const updateRunEntry = (time: string, entries: run[], matchIndex: number) => {
+    entries[matchIndex] = { ...entries[matchIndex], LastRunTimeStamp: time }
+    return entries
+}
+
+const newEntry = (user: string, time: string, entries: run[]) => {
+    let rawEntry: run = {
+        UserName: user,
+        LastRunTimeStamp: time
+    }
+    entries.push(rawEntry)
+    return entries
+}
+
+const saveChanges = (file: fs.PathLike, fileContents: run[]) => {
+    let json = JSON.stringify(fileContents)
+    fs.appendFile(file, json, { flag: 'w+', encoding: 'utf8' }, error => {
+        if (error) {
+            console.log(error)
+            return
+        }
+    })
+}
+
+const initialRun = (file: fs.PathLike, user: string, time: string) => {
+    const entries: run[] = []
+    const rawEntry: run = {
+        UserName: user,
+        LastRunTimeStamp: time
+    }
+    entries.push(rawEntry)
+    const content = entries
+    saveChanges(file, content)
+    getGists(user).then()
+}
+
+const consecutiveRun = (file: fs.PathLike, user: string, time: string) => {
+    const runData = loadRunFile(file)
+    const matchIndex = runData.findIndex(runEntry => runEntry.UserName == user)
+    if (matchIndex > -1) {
+        const updatedContent = updateRunEntry(time, runData, matchIndex)
+        saveChanges(file, updatedContent)
+        options.freshrun ? getGists(user).then() : getGists(user, time).then()
+    } else {
+        const newContent = newEntry(user, time, runData)
+        saveChanges(file, newContent)
+        getGists(user).then()
+    }
+}
+
+// Check if run file exists
+
+if (runFileExists(filePath)) {
+    consecutiveRun(filePath, options.user, timeNow)
+} else {
+    initialRun(filePath, options.user, timeNow)
+}
